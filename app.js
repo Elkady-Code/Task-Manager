5b6e2d454612b351f91d5a84cd8857910c452250
unknown <ibrahimallie905@gmail.com>
2024-07-18 02:20:50 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasksRoute");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/notFound");
+const errorHandlerMiddleware = require("./middleware/errorhandler");
+
+app.use(express.static("./public"));
+app.use(express.json());
+
+app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
